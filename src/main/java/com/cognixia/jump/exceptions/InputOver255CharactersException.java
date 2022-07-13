7a896982c676ffce1e417f1a6f1949c43a38993a
Jacob Laws <jalaws1s@semo.edu>
2022-07-13 15:18:50 -0500

    changed parameter from String to int

--- src/main/java/com/cognixia/jump/exceptions/InputOver255CharactersException.java
+++ src/main/java/com/cognixia/jump/exceptions/InputOver255CharactersException.java
@@ -7,8 +7,8 @@ public class InputOver255CharactersException extends Exception{
 
 	private static final int stringLength = 255;
 	
-	public InputOver255CharactersException(String input) {
-		super("String over 255 characters (VARCHAR max length). Attempted input is " + input
+	public InputOver255CharactersException(int i) {
+		super("String over 255 characters (VARCHAR max length). Attempted input is " + i
 				+ " characters long when max accepted length is " + stringLength + " characters long.");
 	}
 	
