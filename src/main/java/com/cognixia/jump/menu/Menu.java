1b39ff5dba27471fef4f206ed6bb9c2800b0d3ea
Jacob Laws <jalaws1s@semo.edu>
2022-07-13 16:00:50 -0500

    added code to close rs1, pstmt, and conn

--- src/main/java/com/cognixia/jump/menu/Menu.java
+++ src/main/java/com/cognixia/jump/menu/Menu.java
@@ -2,6 +2,7 @@ package com.cognixia.jump.menu;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -19,7 +20,7 @@ public class Menu {
 	public static void main(String[] args) {
 		
 		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		ResultSet rs1 = null;
 		String query = "";
 		Scanner scan = new Scanner(System.in);
 	
@@ -31,85 +32,106 @@ public class Menu {
 		System.out.println("==========================================================");
 		System.out.println();
 
-		while (cond) {
+		try {
 			
-			LoginOptions();
-			int response = getResponse(log_in_options, scan);
-
-			switch (response) {
-			
-			case 1:
+			while (cond) {
 				
-				scan.nextLine();
-				String getUsername = "";
-				String getPassword = "";
+				LoginOptions();
+				int response = getResponse(log_in_options, scan);
+	
+				switch (response) {
 				
-				try {
+				case 1:
 					
-					System.out.println("Enter username: ");
-					getUsername = scan.nextLine();
-					
-					if (getUsername.length() > 255)
-						throw new InputOver255CharactersException(getUsername.length());
-					
-					System.out.println("Enter password: ");
 					scan.nextLine();
-					getPassword = scan.nextLine();
+					String getUsername = "";
+					String getPassword = "";
 					
-					if (getPassword.length() > 255)
-						throw new InputOver255CharactersException(getPassword.length());
-					
-				} catch (InputOver255CharactersException e){
-					
-					System.out.println("Input over max length (255 characters)");
-					
-				}
-				
-				try {
-					
-					query = "select * from user where username = username";
-					PreparedStatement pstmt1 = conn.prepareStatement("select username, password from `user` where username = ?;");
-					pstmt1.setString(1, getUsername);
-					ResultSet rs1 = pstmt1.executeQuery(query);
-					
-					while(rs1.next()) {
+					try {
 						
-					String user_name = rs1.getString("username");
-					String pass_word = rs1.getString("password");
-					
-					if (getUsername == user_name && getPassword == pass_word) {
+						System.out.println("Enter username: ");
+						getUsername = scan.nextLine();
 						
-						System.out.println("Log In Successful");
-						SignedInlistOptions();
+						if (getUsername.length() > 255)
+							throw new InputOver255CharactersException(getUsername.length());
+						
+						System.out.println("Enter password: ");
+						scan.nextLine();
+						getPassword = scan.nextLine();
+						
+						if (getPassword.length() > 255)
+							throw new InputOver255CharactersException(getPassword.length());
+						
+					} catch (InputOver255CharactersException e){
+						
+						System.out.println("Input over max length (255 characters)");
 						
 					}
-					
-					else {
+					try {
 						
-							System.out.println("Log in not successful! Please try again!");
-							LoginOptions();
+						query = "select * from user where username = username";
+						PreparedStatement pstmt1 = conn.prepareStatement("select username, password from `user` where username = ?;");
+						pstmt1.setString(1, getUsername);
+						rs1 = pstmt1.executeQuery(query);
+						
+						while(rs1.next()) {
 							
+						String user_name = rs1.getString("username");
+						String pass_word = rs1.getString("password");
+						
+						if (getUsername == user_name && getPassword == pass_word) {
+							
+							System.out.println("Log In Successful");
+							SignedInlistOptions();
+							
+						}
+						
+						else {
+							
+								System.out.println("Log in not successful! Please try again!");
+								LoginOptions();
+								
+						}
+						
+						break;
+						
+						}
+						
 					}
 					
+						
+						
+					catch (Exception e) {
+						e.printStackTrace();
+					}
+					
+				case 2:
 					break;
-					
-					}
-					
+				case 3:
+					break;
+	
 				}
-				
-					
-					
-				catch (Exception e) {
-					e.printStackTrace();
-				}
-				
-			case 2:
-				break;
-			case 3:
-				break;
-
+	
 			}
-
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			
+		} finally {
+			
+			try {
+				
+				rs1.close();
+				pstmt.close();
+				conn.close();
+				
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+				
+			}
+			
 		}
 
 	}
