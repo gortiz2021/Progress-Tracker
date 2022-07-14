b47f9af619169c647301ea3bac423b09506cd964
gortiz2021 <iGabriel661@hotmal.com>
2022-07-14 12:06:29 -0700

    added an update method that checks the userId and showId and then allows to update the progress

--- src/main/java/com/cognixia/jump/dao/UDAO.java
+++ src/main/java/com/cognixia/jump/dao/UDAO.java
@@ -14,10 +14,40 @@ public class UDAO {
 	private Connection conn = ConnectionManager.getConnection();
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
-	String query = "";
-	U u = null;
+	String query = "";	
 	
-	
+	//	//update to db
+	public U updateProgressOfTvShow(int progress, int userId, int showId) {
+		
+		U u = null;
+		
+		try {
+			
+			query = "update user_tv_show\r\n"
+					+ "set progress = ?\r\n"
+					+ "where user_id = ? and show_id = ?;";
+			
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, progress);
+			pstmt.setInt(2, userId);
+			pstmt.setInt(3, showId);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				u.setProgress(rs.getInt(1));
+				u.setUserId(rs.getInt(2));
+				u.setTvShowId(rs.getInt(3));
+			}
+		}
+		
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return u;
+		
+	}
 	//get one by id
 //	U findbyId(int id);
 //	
@@ -26,8 +56,7 @@ public class UDAO {
 //	
 //	//add to db
 //	public void addUser();
-//	//update to db
-//	boolean update(U entity);
+
 //	//delete by id
 //	boolean deleteById(int id);
 //
