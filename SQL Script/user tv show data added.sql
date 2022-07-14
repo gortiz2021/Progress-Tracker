4e9ed317fc442a7f517fdfecdea859218574773d
Jacob Laws <jalaws1s@semo.edu>
2022-07-14 11:24:13 -0500

    added administrator user to db and user_tv_show data RUN ADMINISTRATOR USER ADDED SQL SCRIPT FIRSTgit add .!

---
+++ SQL Script/user tv show data added.sql
@@ -0,0 +1,66 @@
+use progress_tracker;
+
+-- Administrators by default have completed ALL tv shows
+insert into user_tv_show values (5, 1, 2);
+insert into user_tv_show values (5, 2, 2);
+insert into user_tv_show values (5, 3, 2);
+insert into user_tv_show values (5, 4, 2);
+insert into user_tv_show values (5, 5, 2);
+insert into user_tv_show values (5, 6, 2);
+insert into user_tv_show values (5, 7, 2);
+insert into user_tv_show values (5, 8, 2);
+insert into user_tv_show values (5, 9, 2);
+insert into user_tv_show values (5, 10, 2);
+insert into user_tv_show values (5, 11, 2);
+
+-- Regular users by default have completed NO tv shows
+insert into user_tv_show values (1, 1, 0);
+insert into user_tv_show values (1, 2, 0);
+insert into user_tv_show values (1, 3, 0);
+insert into user_tv_show values (1, 4, 0);
+insert into user_tv_show values (1, 5, 0);
+insert into user_tv_show values (1, 6, 0);
+insert into user_tv_show values (1, 7, 0);
+insert into user_tv_show values (1, 8, 0);
+insert into user_tv_show values (1, 9, 0);
+insert into user_tv_show values (1, 10, 0);
+insert into user_tv_show values (1, 11, 0);
+
+-- Regular users by default have completed NO tv shows
+insert into user_tv_show values (2, 1, 0);
+insert into user_tv_show values (2, 2, 0);
+insert into user_tv_show values (2, 3, 0);
+insert into user_tv_show values (2, 4, 0);
+insert into user_tv_show values (2, 5, 0);
+insert into user_tv_show values (2, 6, 0);
+insert into user_tv_show values (2, 7, 0);
+insert into user_tv_show values (2, 8, 0);
+insert into user_tv_show values (2, 9, 0);
+insert into user_tv_show values (2, 10, 0);
+insert into user_tv_show values (2, 11, 0);
+
+-- Regular users by default have completed NO tv shows
+insert into user_tv_show values (3, 1, 0);
+insert into user_tv_show values (3, 2, 0);
+insert into user_tv_show values (3, 3, 0);
+insert into user_tv_show values (3, 4, 0);
+insert into user_tv_show values (3, 5, 0);
+insert into user_tv_show values (3, 6, 0);
+insert into user_tv_show values (3, 7, 0);
+insert into user_tv_show values (3, 8, 0);
+insert into user_tv_show values (3, 9, 0);
+insert into user_tv_show values (3, 10, 0);
+insert into user_tv_show values (3, 11, 0);
+
+-- Regular users by default have completed NO tv shows
+insert into user_tv_show values (4, 1, 0);
+insert into user_tv_show values (4, 2, 0);
+insert into user_tv_show values (4, 3, 0);
+insert into user_tv_show values (4, 4, 0);
+insert into user_tv_show values (4, 5, 0);
+insert into user_tv_show values (4, 6, 0);
+insert into user_tv_show values (4, 7, 0);
+insert into user_tv_show values (4, 8, 0);
+insert into user_tv_show values (4, 9, 0);
+insert into user_tv_show values (4, 10, 0);
+insert into user_tv_show values (4, 11, 0);
