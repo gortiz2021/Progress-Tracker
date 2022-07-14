2c3ee695bf3cbc25c2bf53ad73996177cfbb27a6
Toju Mikie <40072570+tojumikie@users.noreply.github.com>
2022-07-14 19:11:48 -0400

    Update UserDAO.java

--- src/main/java/com/cognixia/jump/dao/UserDAO.java
+++ src/main/java/com/cognixia/jump/dao/UserDAO.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
+import java.sql.Statement;
 
 import com.cognixia.jump.Menu;
 import com.cognixia.jump.connection.ConnectionManager;
@@ -90,6 +90,43 @@ public class UserDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		String query3 = "select count(*) from tv_show";
+		String getUserIDStr = "select user_id from user where username = ?";
+		int numTVShows = 0;
+		int getUserIDNum = 0;
+		try {
+			Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs = stmt.executeQuery(query3);
+			while(rs.next()) {
+//				System.out.println(rs.getInt(1));
+				numTVShows = rs.getInt(1);
+			}
+			PreparedStatement pstmt4 = conn.prepareStatement(getUserIDStr);
+			pstmt4.setString(1, username);
+			ResultSet rs2 = pstmt4.executeQuery();
+			while(rs2.next()) {
+				getUserIDNum = rs2.getInt("user_id");
+//				System.out.println("user id!!!! " + getUserIDNum);				
+			}
+			
+			
+			String query4 = "insert into user_tv_show values(?, ?, ?)";
+//			int id = getUserIDNum;
+			int showID = 1;
+			int watched = 0;
+			for(; showID <= numTVShows; showID++) {
+				PreparedStatement pstmt3 = conn.prepareStatement(query4);
+				pstmt3.setInt(1, getUserIDNum);
+				pstmt3.setInt(2, showID);
+				pstmt3.setInt(3, watched);
+				pstmt3.executeUpdate();
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
   
 }
