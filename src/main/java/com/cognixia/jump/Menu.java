6766df99db6518d350a078b4b3ceddffe98e82fd
Jacob Laws <jalaws1s@semo.edu>
2022-07-14 15:58:56 -0500

    added extra stuff

--- src/main/java/com/cognixia/jump/Menu.java
+++ src/main/java/com/cognixia/jump/Menu.java
@@ -284,7 +284,6 @@ public class Menu {
 
 				Scanner scan = new Scanner(System.in);
 				int response = getResponse(signed_in_options, scan);
-<<<<<<< HEAD
 				String query = "";
 				
 				
@@ -372,81 +371,9 @@ public class Menu {
 					case 4:
 					default:
 						exit(0);
-							
-						
-=======
-
-				switch (response) {
-
-				case 1:
-					String query = "insert into tv_show values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-					String getLastID = "select MAX(tv_show_id) from tv_show";
-
-					PreparedStatement pstmt = conn.prepareStatement(getLastID);
-					ResultSet rs = pstmt.executeQuery();
-					int lastID = -1;
-
-					if (rs.next()) {
-
-						lastID = rs.getInt(1);
-
-						PreparedStatement pstmt2 = conn.prepareStatement(query);
-						String name = "", actor = "", director = "", genre = "", rating = "", firstEpisode = "",
-								status = "";
-						int numOfSeasons = 0, numOfEpisodes = 0, audienceScore = 0;
-
-						System.out.println("Please enter the show's name:");
-						name = Menu.scan.nextLine();
-						System.out.println("Please enter the show's leading actor:");
-						actor = Menu.scan.nextLine();
-						System.out.println("Please enter the show's director:");
-						director = Menu.scan.nextLine();
-						System.out.println("Please enter the show's number of seasons:");
-						numOfSeasons = Menu.scan.nextInt();
-						System.out.println("Please enter the show's number of episodes:");
-						numOfEpisodes = Menu.scan.nextInt();
-						System.out.println("Please enter the show's genre:");
-						Menu.scan.nextLine();
-						genre = Menu.scan.nextLine();
-						System.out.println("Please enter the show's audience score:");
-						audienceScore = Menu.scan.nextInt();
-						System.out.println("Please enter the show's rating:");
-						Menu.scan.nextLine();
-						rating = Menu.scan.nextLine();
-						System.out.println("Please enter the show's first episode's name:");
-						firstEpisode = Menu.scan.nextLine();
-						System.out.println("Please enter the show's status:");
-						status = Menu.scan.nextLine();
-
-						pstmt2.setInt(1, lastID + 1);
-						pstmt2.setString(2, name);
-						pstmt2.setString(3, actor);
-						pstmt2.setString(4, director);
-						pstmt2.setInt(5, numOfSeasons);
-						pstmt2.setInt(6, numOfEpisodes);
-						pstmt2.setString(7, genre);
-						pstmt2.setInt(8, audienceScore);
-						pstmt2.setString(9, rating);
-						pstmt2.setString(10, firstEpisode);
-						pstmt2.setString(11, status);
-
-						pstmt2.executeUpdate();
-						System.out.println("Added topic.");
-
->>>>>>> 3562e5730b1e89aae4b3b454b4f295ecc42dea55
 					}
-					break;
-				case 2:
-					System.out.println();
-				case 3:
-					System.out.println();
-				case 4:
-				default:
-					exit(0);
 
 				}
-
-			}
 
 		} catch (Exception e) {
 
