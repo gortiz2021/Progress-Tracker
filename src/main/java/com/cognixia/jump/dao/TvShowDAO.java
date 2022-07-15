ce0e0e1304eca76cdc74f200aece39884f92080c
gortiz2021 <iGabriel661@hotmal.com>
2022-07-15 08:30:01 -0700

    Added comments to TvShow.java

--- src/main/java/com/cognixia/jump/dao/TvShowDAO.java
+++ src/main/java/com/cognixia/jump/dao/TvShowDAO.java
@@ -15,24 +15,33 @@ public class TvShowDAO {
 	
 	// List functionality we want to have to get data from our DB to our Models.
 
-
+	// Initialize variables outside of methods to avoid redundant code 
 	private Connection conn = ConnectionManager.getConnection();
 	PreparedStatement pstmt = null;
 	ResultSet rs = null;
 	String query = "";
 	
+	// Will create a TvShow object and takes in a id
+	// will return a single tv show based on the inputed 
+	// tv show id
 	public TvShow getTvShow (int id){
 		
 		TvShow show = null;
 	
 		try {
+			// SQL query that is saved as a prepared statement 
 			query = "select * from tv_show where tv_show_id = ?;";
 			
 			pstmt = conn.prepareStatement(query);
+			// set the parameter index (what "?" you are selecting), then
+			// the value you want to input in that place
 			pstmt.setInt(1, id);
-			
+
+			// save the prepared statement inside a result set
 			rs = pstmt.executeQuery();
 			
+			// goes through the result set and stores the information taken from the 
+			// database which is then saved inside a TvShow object named "show"
 			if(rs.next()) {
 				show.setId(rs.getInt(1));
 				show.setName(rs.getString(2));
@@ -47,10 +56,12 @@ public class TvShowDAO {
 				show.setStatus(rs.getString(11));
 			}
 		}
+		// avoid empty catch blocks
 		catch (Exception e){
 			e.printStackTrace();
 		}
 			
+		// return the object that stored all the info
 		return show;
 	}
 	
