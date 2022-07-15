83c7bd9b1ab01c82de6131da2b9e4c21420e59ef
gortiz2021 <iGabriel661@hotmal.com>
2022-07-15 08:06:55 -0700

    Added comments to U.java, TvShow.java, and User.java

--- src/main/java/com/cognixia/jump/model/User.java
+++ src/main/java/com/cognixia/jump/model/User.java
@@ -1,21 +1,15 @@
 package com.cognixia.jump.model;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import com.cognixia.jump.connection.ConnectionManager;
-import com.cognixia.jump.exceptions.UserAlreadyExistsException;
-import com.cognixia.jump.Menu;
-
+// User model is to be used later in the UserDao class
 public class User{
-	Connection conn = ConnectionManager.getConnection();
-
+	
+	// Variables come from the columns in the table named "user"
+	// The table is in the "progress_checker" database
 	private int user_id;
 	private String username;
 	private String password;
 	
+	// Two default constructors that are to create new objects of this class
 	public User() {
 		this(-1, "N/A", "N/A");
 	}
@@ -24,10 +18,10 @@ public class User{
 		super();
 		this.user_id = user_id;
 		this.username = username;
-		this.password = password;
-		
+		this.password = password;		
 	}
-
+	
+	// A list of auto-generated getter and setter functions for every variable 
 	public long getUser_id() {
 		return user_id;
 	}
@@ -53,43 +47,9 @@ public class User{
 		this.password = password;
 	}
 		
-
+	// Overriden toString() method to display any object of this class
 	@Override
 	public String toString() {
 		return "User [user_id=" + user_id + ", username=" + username + ", password=" + password + "]";
 	}
-	public void addUser() {
-		String query = "insert into user (username, password) values (?, ?)";
-		String check = "select * from user where username = ?";
-		System.out.println("CREATE A USER:");
-		System.out.println("please enter your username");
-		username = Menu.scan.nextLine();
-		System.out.println("Please enter your password");
-		password = Menu.scan.nextLine();
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(query);
-			pstmt.setString(1, username);
-			pstmt.setString(2, password);
-			PreparedStatement pstmt2 = conn.prepareStatement(check);
-			pstmt2.setString(1, username);
-//			pstmt2.setString(2, password);
-//			int i = pstmt2.executeUpdate();
-			ResultSet rs = pstmt2.executeQuery();
-			
-//			System.out.println(rs);
-			if (rs.next() == false) {
-				int i = pstmt.executeUpdate();
-				System.out.println("user created with username: " + username);				
-			}
-			else {
-				throw new UserAlreadyExistsException("user already exists!");
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (UserAlreadyExistsException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
 }
