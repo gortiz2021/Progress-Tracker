eb1202472fa50302ad7c0696c0ab614bd98bcc71
Toju Mikie <40072570+tojumikie@users.noreply.github.com>
2022-07-15 10:18:52 -0400

    Update UserAlreadyExistsException.java

--- src/main/java/com/cognixia/jump/exceptions/UserAlreadyExistsException.java
+++ src/main/java/com/cognixia/jump/exceptions/UserAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package com.cognixia.jump.exceptions;
-
+/**
+ * Custom Exception to give a message that the user already exists. The message is passed by the UserDAO.addUser() method.
+ * @authors Jacob Laws, Michelle Lovse, Gabriel Ortiz, Toju Mikie
+ *
+ */
 public class UserAlreadyExistsException extends Exception{
 	public UserAlreadyExistsException(String message) {
 		super(message);
